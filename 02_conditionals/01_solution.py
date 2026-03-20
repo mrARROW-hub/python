8dc4af5d9dac1e822df272b8138ce38bbc4c5b42
ARROW <rishabbhkumar0002@gmail.com>
2026-03-20 03:18:26 +0000

    02_conditionals/01_solution.py

---
+++ 02_conditionals/01_solution.py
@@ -0,0 +1,11 @@
+user_age = 9
+
+if user_age < 13:
+    print("Child ^__^");
+
+elif user_age < 20:
+    print("teenager ＼(ﾟｰﾟ＼)");
+elif user_age < 60:
+    print("adult :D");
+else:
+    print("Senior citizen :/");
