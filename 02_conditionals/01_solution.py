65adf87b753191086bfbc010419ecfd1af59cd8b
ARROW <rishabbhkumar0002@gmail.com>
2026-03-20 04:26:51 +0000

    02_conditionals/01_solution.py

--- 02_conditionals/01_solution.py
+++ 02_conditionals/01_solution.py
@@ -1,10 +1,10 @@
-user_age = 9
+user_age = int(input("Tell us your age :D : "));
 
 if user_age < 13:
     print("Child ^__^");
 
 elif user_age < 20:
-    print("teenager ＼(ﾟｰﾟ＼)");
+    print("teenager  \(ﾟｰﾟ＼)");
 elif user_age < 60:
     print("adult :D");
 else:
---
+++ 02_conditionals/03_solution.py
@@ -0,0 +1,16 @@
+student_score = int(input("Pls tell us your grades!: "));
+
+if student_score > 100:
+    print("Recheck your grades DUMB :/");
+
+elif student_score > 89:
+    print("A grade!! :D");
+
+elif student_score > 79:
+    print("B Grade!! :)");
+elif student_score > 69:
+    print("C Grade!! :/");
+elif student_score > 59:
+    print("D Grade!! nearly passed DUMB ಠ_ಠ");
+else:
+    print("You are FAIL dammit!!!! (●__●)");
