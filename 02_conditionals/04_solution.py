ca659e6a88d8eb24cb50dea59b9c372562621705
ARROW <rishabbhkumar0002@gmail.com>
2026-03-22 07:49:48 +0000

    02_conditionals/04_solution.py

---
+++ 02_conditionals/04_solution.py
@@ -0,0 +1,8 @@
+weather = str(input("what is the weather today :) :"));
+
+if weather == "sunny":
+    print("Go for a walk today :)");
+elif weather == "rainy":
+    print("read a book!");
+elif weather == "snowy":
+    print("build a dwarf snow man like you!!!");
