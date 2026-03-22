71dd6621208a950fae579a9c6681a8a1333826b1
ARROW <rishabbhkumar0002@gmail.com>
2026-03-22 08:26:06 +0000

    02_conditionals/06_solution.py

---
+++ 02_conditionals/06_solution.py
@@ -0,0 +1,9 @@
+distance = int(input("tell us the distance you want to travel :"));
+
+if distance <= 3:
+    print("you should walk");
+elif distance <= 15:
+    print("Go by your bike");
+elif distance > 15:
+    print("use your Car");
+
