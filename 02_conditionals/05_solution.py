b2a879c8e7611a3512fdcbebd143080c7a7c5e3c
ARROW <rishabbhkumar0002@gmail.com>
2026-03-22 07:58:26 +0000

    02_conditionals/05_solution.py

---
+++ 02_conditionals/05_solution.py
@@ -0,0 +1,11 @@
+fruit = str(input("what fruit do you want to check to check :"));
+
+fruit_color = str(input("tell me the color of your fuit :"));
+
+if fruit == "banana":
+    if fruit_color == "yellow":
+        print("ripe!");
+    elif fruit_color == "green":
+        print("underripe");
+    elif fruit_color == "brown":
+        print("overripe");
